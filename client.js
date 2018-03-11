4b5a6607048def174a2bbf0a833104c84f3ededc
Tenzin Chosang <TenzinChosang@Tenzins-Air.Home>
2018-03-11 18:40:40 -0500

    need to remove when clicked

--- client.js
+++ client.js
@@ -2,22 +2,50 @@
 $( document ).ready(readyNow);
 
 let yearlyCost = 0;
-let monthlyCost =0;
+let monthlyCost = 0;
+let employeeList= [];
+
+class Employee {
+  constructor( uno, dos, tres, cuatro, cinco){
+    this.firstName = uno;
+    this.lastName = dos;
+    this.id = tres;
+    this.jobTitle = cuatro;
+    this.salary = cinco;
+  }
+}
 
 function readyNow(){
   $('#addEmployeeButton').on('click', submitInfo);
+  $('#content').on('click', ".ripEmployee", theyGone);
 }
 
 function submitInfo(){
-  let uno = $('#firstIn').val();
-  let dos = $('#lastIn').val();
-  let tres = $('#idIn').val();
-  let quatro = $('#titleIn').val();
-  let cinco = $('#salaryIn').val();
-  $('#blankPage').append('<div>', uno," ", dos," ", tres," ", quatro," ", cinco, '</div>');
+  if($('#firstIn').val() == "" || $('#lastIn').val() == "" || $('#idIn').val() == "" || $('#titleIn').val() == "" || $('#salaryIn').val() == ""){
+    return false;
+  }
+  addToArray();
+  appendToDom();
   zeCostMonth();
   emptyInput();
   checkCost();
+}
+
+function theyGone(){
+  $(this).remove();
+}
+
+
+function appendToDom(){
+  let outputElement = $('#content');
+  outputElement.empty();
+  for (let employee of employeeList){
+    outputElement.append('<tr><td>' + employee.firstName +'</td><td>' + employee.lastName + '</td><td>' + employee.id + '</td><td>' + employee.jobTitle +'</td><td>'+ employee.salary + '</td><td>' + '<button class = ripEmployee>Terminate</button>' + '</td></tr>');
+  }
+}
+
+function addToArray(){
+  employeeList.push(new Employee($('#firstIn').val(), $('#lastIn').val(), $('#idIn').val(), $('#titleIn').val(), $('#salaryIn').val() ));
 }
 
 function checkCost(){
